f679139d7200d1b4193fac339dd83478f76f7bff
Robson Natanael <natanaelrobson@gmail.com>
2020-11-25 23:29:28 -0300

    Recrie o arquivo config.install.php

---
+++ app/config/config.install.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * 2020 - RN Comunicação & Marketing
+ *
+ * AVISO DE LICENÇA
+ *
+ * Este arquivo de origem está sujeito à Licença MIT
+ * incluído neste pacote no arquivo LICENSE.txt.
+ * Também está disponível na Internet neste URL:
+ * https://opensource.org/licenses/MIT
+ *
+ * @author Robson Natanael <natanaelrobson@gmail.com>
+ * @copyright 2020 - RN Comunicação & Marketing
+ * @license https://opensource.org/licenses/MIT MIT License
+ *
+ * @package Contact Module
+ */
+
+// URI Base
+define("URI", "htpps://example.com");
+
+// Configuração SMTP
+define("HOST", "smtp.example.com");
+define("PORT", 587);
+define("SECURITY", "tls");
+define("SMTP_AUTH", true);
+define("USERNAME", "user@example.com");
+define("PASSWORD", "secret");
+define("NAME", "Name");
+
+// E-mail para notificação
+define("EMAIL_NOTIFICACAO", "user@example.com");
+
+// Chaves de reCAPTCHA
+define("SITE_KEY", "key");
+define("SECRET_KEY", "secret");
