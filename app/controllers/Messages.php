471c214c1842e54c0b873159ff1980ce058db068
Robson Natanael <natanaelrobson@gmail.com>
2020-12-23 20:20:34 -0300

    Unifique as classes MessagesList e MessageView em Messages

--- app/controllers/Messages.php
+++ app/controllers/Messages.php
@@ -26,9 +26,9 @@ if (!defined('RN2020')) {
     die('Página não encontrada!');
 }
 
-class MessagesList
+class Messages
 {
-    public static function index()
+    public static function list()
     {
 
         Transaction::open('database');
@@ -54,5 +54,34 @@ class MessagesList
         $parameters['chat_list'] = $chat_list;
 
         AppLoader::load('messages.html', $parameters);
+    }
+
+    public static function view()
+    {
+
+        Transaction::open('database');
+
+        $messages = Message::all('id_chat = ' . $_GET['param']);
+        $msg = array();
+        $msg['id_chat'] = $_GET['param'];
+
+        $chat = Chat::find($_GET['param']);
+
+        $msg['subject'] = $chat->subject;
+
+        $chat_view = array();
+        foreach ($messages as $message) {
+
+            $user = User::find($message->id_user);
+            $chat_view[$message->id]['id_user'] = $user->id;
+            $chat_view[$message->id]['user'] = $user->name;
+            $chat_view[$message->id]['message'] = $message->message;
+            $chat_view[$message->id]['date_send'] = $message->date_send;
+        }
+
+        Transaction::close();
+        $msg['chat'] = $chat_view;
+
+        AppLoader::load('message-view.html', $msg);
     }
 }
